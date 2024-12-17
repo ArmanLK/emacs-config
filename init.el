8ed1383476e16cd1735d5aafc47b693898e799eb
ArmanLK <cheshmiarman@gmail.com>
2024-12-17 16:10:37 +0330

    moved eglot thing to a new file

--- init.el
+++ init.el
@@ -14,6 +14,10 @@
 (when (file-exists-p custom-file)
   (load custom-file))
 
+(setq lsp-file(concat user-emacs-directory "lsp.el"))
+(when (file-exists-p lsp-file)
+  (load lsp-file))
+
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
@@ -25,47 +29,3 @@
 (setq gc-cons-threshold most-positive-fixnum)
 
 (eldoc-mode -1)
-
-(add-hook 'emacs-startup-hook
-	  (lambda ()
-	    (setq gc-cons-threshold (expt 2 23))))
-
-(add-hook 'emacs-lisp-mode-hook
- 	  (lambda ()
-	    (rainbow-delimiters-mode-enable)
-	    (company-mode)))
-
-(add-hook 'c-mode-hook
-	  (lambda()
-	    (eglot-ensure)
-	    (eglot-inlay-hints-mode nil)))
-
-(add-hook 'c++-mode-hook
-	  (lambda()
-	    (eglot-ensure)
-	    (eglot-inlay-hints-mode nill)))
-
-(add-hook 'zig-mode-hook
-	  (lambda()
-	    (eglot-ensure)
-	    (eglot-inlay-hints-mode nill)))
-
-(add-hook 'go-mode-hook
-	  (lambda()
-	    (eglot-ensure)
-	    (eglot-inlay-hints-mode nil)))
-
-(add-hook 'python-mode-hook
-	  (lambda()
-	    (eglot-ensure)
-	    (eglot-inlay-hints-mode nil)))
-
-(add-hook 'rust-mode-hook
-	  (lambda()
-	    (eglot-ensure)
-	    (eglot-inlay-hints-mode nill)))
-
-(add-hook 'tex-mode-hook
-	  (lambda ()
-	    (company-mode)
-	    (eglot-ensure)))
---
+++ lsp.el
@@ -0,0 +1,67 @@
+(add-hook 'emacs-startup-hook
+	  (lambda ()
+	    (setq gc-cons-threshold (expt 2 23))))
+
+(add-hook 'emacs-lisp-mode-hook
+	  (lambda ()
+	    (rainbow-delimiters-mode-enable)
+	    (company-mode)))
+
+(add-hook 'c-mode-hook
+	  (lambda()
+	    (eglot-ensure)
+	    (eglot-inlay-hints-mode nil)))
+
+(add-hook 'c++-mode-hook
+	  (lambda()
+	    (eglot-ensure)
+	    (eglot-inlay-hints-mode nill)))
+
+(add-hook 'zig-mode-hook
+	  (lambda()
+	    (eglot-ensure)
+	    (eglot-inlay-hints-mode nill)))
+
+(add-hook 'go-mode-hook
+	  (lambda()
+	    (eglot-ensure)
+	    (eglot-inlay-hints-mode nil)))
+
+(add-hook 'python-mode-hook
+	  (lambda()
+	    (eglot-ensure)
+	    (eglot-inlay-hints-mode nil)))
+
+(add-hook 'rust-mode-hook
+	  (lambda()
+	    (eglot-ensure)
+	    (eglot-inlay-hints-mode nill)))
+
+(add-hook 'tex-mode-hook
+	  (lambda ()
+	    (company-mode)
+	    (eglot-ensure)))
+
+(use-package eglot
+  :config
+  (add-to-list 'eglot-server-programs
+	       '(typescript-mode . ("npx"
+				    "typescript-language-server"
+				    "--stdio"))))
+(use-package eglot
+  :config
+  (add-to-list 'eglot-server-programs
+	       '(js-mode . ("npx"
+			    "typescript-language-server"
+			    "--stdio"))))
+
+
+(add-hook 'js-mode
+	  (lambda ()
+	    (company-mode)
+	    (eglot-ensure)))
+
+(add-hook 'typescript-mode
+	  (lambda ()
+	    (company-mode)
+	    (eglot-ensure)))
